0256967c50792073c5a5b0a37d89eaf247f3d431
yasser-aboufaris <azertyasser321@gmail.com>
2025-09-24 11:52:49 +0100

    init: create the initial project structure

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int choice ;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("hello");
+        String data = scanner.next();
+        data = (data) -> {return "test";};
+
+        System.out.println(data);
+
+
+        }
+    }
+6
